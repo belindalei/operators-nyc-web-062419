8af6d1c724dd9cc988fec121858f16c5c630f763
Belinda Lei <belinda.lei95@gmail.com>
2019-06-02 20:44:46 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,14 @@
 def unsafe?(speed)
-   speed > 60 ? 
-   true : false 
-   speed < 40 ? 
-   true : false 
+   if speed > 60 
+     true 
+   else speed < 40 
+     false 
 end
 
 
 
 def not_safe?(speed)
-	speed > 60 ? 
-  true : false 
-  speed < 40 ? 
+	speed > 60 || speed < 40 ? 
   true : false 
 end
 	
